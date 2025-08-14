dbdb5d31cc8c2cb112541c0aa85a896cf234b725
PrinceAmoakoBannerman <princebannerman17@gmail.com>
2025-08-14 12:39:17 +0000

    Fix volunteer login redirect to correct login URL

--- accounts/views.py
+++ accounts/views.py
@@ -98,7 +98,7 @@ def donate_view(request):
 
 from django.contrib.auth.decorators import login_required
 
-@login_required(login_url='/accounts/login/')
+@login_required(login_url='/login/')
 def volunteer_view(request):
     context = {}
     if request.method == 'POST':
